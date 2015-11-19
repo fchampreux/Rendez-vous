dc4451a5216f0a1d2bf5368cd175685941d24297
Fred <fred@opendataquality.com>
2015-11-19 16:19:44 +0100

    WIP schema

--- db/schema.rb
+++ db/schema.rb
@@ -7,6 +7,8 @@ class CreateBasicTables < ActiveRecord::Migration
       t.text     :address       , limit: 1000
       t.string   :zip           , limit: 20
       t.string   :city          , limit: 100
+      t.string   :state         , limit: 100
+      t.integer  :country_id   
       t.string   :website       , limit: 100
       t.string   :email         , limit: 100
       t.string   :phone         , limit: 100
@@ -21,15 +23,20 @@ class CreateBasicTables < ActiveRecord::Migration
     end
     
     create_table :people do |t|
-      t.string   :name          , limit: 100, null: false, index: true
-      t.integer  :title_id	
+      t.integer  :title_id
+      t.string   :first_name    , limit: 100
+      t.string   :mid_name      , limit: 100
+      t.string   :last_name     , limit: 100, null: false, index: true
       t.text     :description   , limit: 4000
       t.text     :address       , limit: 1000
       t.string   :zip           , limit: 20
       t.string   :city          , limit: 100
+      t.string   :state         , limit: 100
+      t.integer  :country_id   
       t.string   :website       , limit: 100
       t.string   :email         , limit: 100
       t.string   :phone         , limit: 100
+      t.string   :mobile        , limit: 100
       t.text     :categories    , limit: 100
       t.integer  :status_id     , null: false, default: 0
       t.integer  :owner_id      , null: false
@@ -45,9 +52,10 @@ class CreateBasicTables < ActiveRecord::Migration
       t.text     :address       , limit: 1000
       t.string   :zip           , limit: 20
       t.string   :city          , limit: 100
+      t.string   :state         , limit: 100
+      t.integer  :country_id   
       t.string   :website       , limit: 100
       t.string   :email         , limit: 100
-      t.string   :phone         , limit: 100
       t.text     :categories    , limit: 100
       t.integer  :status_id     , null: false, default: 0
       t.integer  :owner_id      , null: false
@@ -60,11 +68,6 @@ class CreateBasicTables < ActiveRecord::Migration
     create_table :actions do |t|
       t.string   :name          , limit: 100, null: false, index: true
       t.text     :description   , limit: 4000
-      t.text     :address       , limit: 1000
-      t.string   :zip           , limit: 20
-      t.string   :city          , limit: 100
-      t.string   :website       , limit: 100
-      t.string   :phone         , limit: 100
       t.datetime :planned_at
       t.datqtime :closing_at
       t.datetime :started_at
@@ -72,7 +75,7 @@ class CreateBasicTables < ActiveRecord::Migration
       t.datetime :due_at
       t.decimal  :duration      , precision: 12, scale: 2
       t.integer  :priority_id   , default: 0
-      t.integer  :type_id       , null: false, default: 0
+      t.integer  :mode_id       , null: false, default: 0
       t.integer  :status_id     , null: false, default: 0
       t.integer  :owner_id      , null: false
       t.datetime   :created_at,      null: false
@@ -109,10 +112,10 @@ class CreateBasicTables < ActiveRecord::Migration
     end
     
     create_table :leads_imports do |t|
-      t.string   :title
-      t.string   :first_name
-      t.string   :mid_name
-      t.string   :name          , limit: 100, null: false, index: true
+      t.string   :title         , limit: 100
+      t.string   :first_name    , limit: 100
+      t.string   :mid_name      , limit: 100
+      t.string   :last_name     , limit: 100, null: false, index: true
       t.text     :description   , limit: 4000
       t.text     :address       , limit: 1000
       t.string   :zip           , limit: 20
@@ -121,28 +124,28 @@ class CreateBasicTables < ActiveRecord::Migration
       t.string   :email         , limit: 100
       t.string   :phone         , limit: 100
       t.text     :categories    , limit: 100
-      t.string   :language
-      t.text     :description
-      t.string   :location
-      t.string   :email
-      t.string   :phone
-      t.string   :url
-      t.text     :note
-      t.string   :image_url
-      t.string   :role
-      t.string   :company
-      t.string   :from_date
-      t.string   :job_location
-      t.text     :job_desc
-      t.string   :industry
-      t.string   :cy_number
-      t.string   :cy_street
-      t.string   :cy_address_info
-      t.string   :zip
-      t.string   :pobox
-      t.string   :cy_state
-      t.string   :cy_country
-      t.string   :action
+      t.string   :language      , limit: 100  
+      t.string   :location      , limit: 100
+      t.string   :email         , limit: 100
+      t.string   :phone         , limit: 100
+      t.string   :url           , limit: 100 
+      t.string   :image_url     , limit: 100
+      t.string   :company       , limit: 100
+      t.text     :cy_desc       , limit: 4000
+      t.text     :cy_address    , limit: 1000
+      t.string   :cy_zip        , limit: 20
+      t.string   :cy_city       , limit: 100
+      t.string   :cy_state      , limit: 100
+      t.string   :cy_country    , limit: 100 
+      t.string   :cy_website    , limit: 100
+      t.string   :cy_email      , limit: 100
+      t.string   :cy_phone      , limit: 100
+      t.string   :role          , limit: 100
+      t.string   :from_date     , limit: 20
+      t.string   :role          , limit: 100
+      t.string   :job_location  , limit: 100
+      t.text     :job_desc      , limit: 100
+      t.string   :action_id     , limit: 100
       t.datetime   :created_at,      null: false
       t.datetime   :updated_at,      null: false
       
